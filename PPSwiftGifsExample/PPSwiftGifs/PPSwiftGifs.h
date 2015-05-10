7376e20549a82c7925328f46ea6c262aa11c7eda
Peter Prokop <prokop.petr@gmail.com>
2015-05-10 11:18:04 +0300

    Add PPSwiftGifs framework & shared scheme

---
+++ PPSwiftGifsExample/PPSwiftGifs/PPSwiftGifs.h
@@ -0,0 +1,19 @@
+//
+//  PPSwiftGifs.h
+//  PPSwiftGifs
+//
+//  Created by Peter Prokop on 10/05/15.
+//  Copyright (c) 2015 Peter Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PPSwiftGifs.
+FOUNDATION_EXPORT double PPSwiftGifsVersionNumber;
+
+//! Project version string for PPSwiftGifs.
+FOUNDATION_EXPORT const unsigned char PPSwiftGifsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPSwiftGifs/PublicHeader.h>
+
+
